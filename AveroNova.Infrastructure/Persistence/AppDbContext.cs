94c266fa009876d29d85522a4027af3d6b8399d0
Fali42261 <faizanali42261@gmail.com>
2026-08-22 15:45:40 +0530

    Register invoice entities in local DbContext

--- AveroNova.Infrastructure/Persistence/AppDbContext.cs
+++ AveroNova.Infrastructure/Persistence/AppDbContext.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AveroNova.Domain.Entities;
+﻿using AveroNova.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace AveroNova.Infrastructure.Persistence
 {
     public class AppDbContext : DbContext
     {
-        //Constructor
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
         }
-        //DbSet Properties
+
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
@@ -26,12 +21,34 @@ namespace AveroNova.Infrastructure.Persistence
         public DbSet<Subscription> Subscriptions { get; set; }
         public DbSet<SubscriptionPlan> SubscriptionPlans { get; set; }
         public DbSet<SubscriptionPlanFeature> SubscriptionPlanFeatures { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
-        }
 
+            modelBuilder.Entity<Invoice>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.HasIndex(x => new { x.CompanyId, x.InvoiceNumber });
+                entity.HasIndex(x => new { x.CompanyId, x.CustomerId });
+                entity.HasOne(x => x.Company)
+                    .WithMany()
+                    .HasForeignKey(x => x.CompanyId)
+                    .OnDelete(DeleteBehavior.Restrict);
+                entity.HasMany(x => x.Items)
+                    .WithOne(x => x.Invoice)
+                    .HasForeignKey(x => x.InvoiceId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<InvoiceItem>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.HasIndex(x => new { x.InvoiceId, x.ProductId });
+            });
+        }
     }
 }
