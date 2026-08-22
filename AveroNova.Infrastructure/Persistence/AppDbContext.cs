2b610b2e43c1b1ab9cde486789f6c18b4cbbabb0
Fali42261 <faizanali42261@gmail.com>
2026-08-22 16:31:40 +0530

    Register purchase entities in local database

--- AveroNova.Infrastructure/Persistence/AppDbContext.cs
+++ AveroNova.Infrastructure/Persistence/AppDbContext.cs
@@ -22,6 +22,8 @@ namespace AveroNova.Infrastructure.Persistence
         public DbSet<Invoice> Invoices { get; set; }
         public DbSet<InvoiceItem> InvoiceItems { get; set; }
         public DbSet<StockMovement> StockMovements { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<PurchaseItem> PurchaseItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -49,6 +51,20 @@ namespace AveroNova.Infrastructure.Persistence
                 entity.HasIndex(x => new { x.CompanyId, x.ProductId, x.CreatedAt });
                 entity.HasOne(x => x.Company).WithMany().HasForeignKey(x => x.CompanyId).OnDelete(DeleteBehavior.Restrict);
                 entity.HasOne(x => x.Product).WithMany().HasForeignKey(x => x.ProductId).OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Purchase>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.HasIndex(x => new { x.CompanyId, x.PurchaseNumber });
+                entity.HasIndex(x => new { x.CompanyId, x.SupplierId });
+                entity.HasMany(x => x.Items).WithOne(x => x.Purchase).HasForeignKey(x => x.PurchaseId).OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<PurchaseItem>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.HasIndex(x => new { x.PurchaseId, x.ProductId });
             });
         }
     }
