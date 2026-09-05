d81ed4d189528853e2387cee89a362db48ca422c
Fali42261 <faizanali42261@gmail.com>
2026-09-05 18:13:58 +0530

    Remove duplicate MAUI app builder from MainLayout

--- AveroNova.App.UI/Layouts/MainLayout.xaml.cs
+++ AveroNova.App.UI/Layouts/MainLayout.xaml.cs
@@ -2,27 +2,8 @@ namespace AveroNova.App.UI.Layouts;
 
 public partial class MainLayout : ContentView
 {
-	public MainLayout()
-	{
-		InitializeComponent();
-	}
-
-
-    public static MauiApp CreateMauiApp()
+    public MainLayout()
     {
-        var builder = MauiApp.CreateBuilder();
-
-        builder
-            .UseMauiApp<App>();
-
-#if WINDOWS
-    Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("Borderless", (handler, view) =>
-    {
-        handler.PlatformView.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
-    });
-#endif
-
-        return builder.Build();
+        InitializeComponent();
     }
-
 }
