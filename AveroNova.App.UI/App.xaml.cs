fc9d03d17c42ea7d15a74ea9a06dee1efdf0dd89
Fali42261 <faizanali42261@gmail.com>
2026-09-05 19:26:14 +0530

    Run expense sync on app startup and resume

--- AveroNova.App.UI/App.xaml.cs
+++ AveroNova.App.UI/App.xaml.cs
@@ -10,58 +10,29 @@ namespace AveroNova.App.UI
         private readonly ISyncService _sync;
         private readonly IProcurementSyncService _procurementSync;
         private readonly IReturnSyncService _returnSync;
+        private readonly IExpenseSyncService _expenseSync;
 
-        public App(
-            AppShell appShell,
-            IConnectivityService connectivity,
-            ISyncService sync,
-            IBillingService billingService,
-            IProcurementSyncService procurementSync,
-            IReturnSyncService returnSync)
+        public App(AppShell appShell,IConnectivityService connectivity,ISyncService sync,IBillingService billingService,
+            IProcurementSyncService procurementSync,IReturnSyncService returnSync,IExpenseSyncService expenseSync)
         {
-            InitializeComponent();
-            _appShell = appShell;
-            _connectivity = connectivity;
-            _sync = sync;
-            _procurementSync = procurementSync;
-            _returnSync = returnSync;
-            _ = billingService;
+            InitializeComponent();_appShell=appShell;_connectivity=connectivity;_sync=sync;_procurementSync=procurementSync;_returnSync=returnSync;_expenseSync=expenseSync;_ = billingService;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            var window = new Window(_appShell)
-            {
-                Title = "AveroNova"
-            };
-
-            window.Created += (_, _) => TriggerSyncIfOnline();
-            window.Activated += (_, _) => TriggerSyncIfOnline();
-
+            var window=new Window(_appShell){Title="AveroNova"};
+            window.Created+=(_,_)=>TriggerSyncIfOnline();window.Activated+=(_,_)=>TriggerSyncIfOnline();
 #if WINDOWS
-            window.Width = 1400;
-            window.Height = 900;
-            window.Created += (sender, args) =>
-            {
-                if (window.Handler?.PlatformView is Microsoft.UI.Xaml.Window nativeWindow)
-                {
-                    if (nativeWindow.AppWindow.Presenter is Microsoft.UI.Windowing.OverlappedPresenter presenter)
-                    {
-                        presenter.Maximize();
-                    }
-                }
-            };
+            window.Width=1400;window.Height=900;
+            window.Created+=(sender,args)=>{if(window.Handler?.PlatformView is Microsoft.UI.Xaml.Window nativeWindow&&nativeWindow.AppWindow.Presenter is Microsoft.UI.Windowing.OverlappedPresenter presenter)presenter.Maximize();};
 #endif
-
             return window;
         }
 
         private void TriggerSyncIfOnline()
         {
-            if (!_connectivity.IsOnline) return;
-            _ = _sync.SyncNowAsync();
-            _ = _procurementSync.SyncPendingAsync();
-            _ = _returnSync.SyncPendingAsync();
+            if(!_connectivity.IsOnline)return;
+            _=_sync.SyncNowAsync();_=_procurementSync.SyncPendingAsync();_=_returnSync.SyncPendingAsync();_=_expenseSync.SyncPendingAsync();
         }
     }
 }
